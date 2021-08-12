2d6e42ccd911b35e096bb7757b679a633aa363b7
HeWenxin <1641190800@qq.com>
2021-08-12 19:19:02 +0800

    完成经纬度计算两点之间直线距离

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -2,14 +2,36 @@ Page({
 
 	data: {
 		"seekRed":"https://636c-cloud1-7g5l3bod16c561b2-1306791508.tcb.qcloud.la/swiper_image/seekRed.jpg?sign=c1cd44e25acea935014640fc96bb4d4a&t=1628431927",
-		"OneHundredBook":"https://636c-cloud1-7g5l3bod16c561b2-1306791508.tcb.qcloud.la/swiper_image/100Book.jpg?sign=953a482626cdb36eccca66622b74fe5c&t=1628432070"
+		"OneHundredBook":"https://636c-cloud1-7g5l3bod16c561b2-1306791508.tcb.qcloud.la/swiper_image/100Book.jpg?sign=953a482626cdb36eccca66622b74fe5c&t=1628432070",
+		"test":""
 	},
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
-
+		var that = this;
+		wx.getLocation({
+			type: 'gcj02',
+			isHighAccuracy:true,
+			success (res) {
+				wx.cloud.callFunction({
+					name:"computeStraightDistance",
+					data:{
+						latFrom:res.latitude,
+						lngFrom:res.longitude,
+						latTo : 24.366833,
+						lngTo : 118.050018
+					},
+					complete:res => {
+						//console.log(res);
+						that.setData({
+							test:res.result + "米"
+						})
+					}
+				});
+			}
+		   });
 	},
 
 	/**
