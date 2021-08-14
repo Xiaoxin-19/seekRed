77d2e340b055025fa897a3ec2fccd6afbe828943
HeWenxin <1641190800@qq.com>
2021-08-14 17:13:44 +0800

    1. 小程序景点列表显示优化 2. 景点列表动态加载 3. 景点详情页面动态加载

--- miniprogram/pages/spot/spot.js
+++ miniprogram/pages/spot/spot.js
@@ -2,12 +2,15 @@ var appInstance = getApp();
 Page({
 	data: {
 		cities:[],
+		spotList:[],
 		currentCityIndex:0,
+		initCityName:"福州市",
 		windowHeight:500,
 	},
-
+	
 	onLoad:function(options){
 		var that = this;
+		var initCityName = 1;
 		//获取城市列表
 		const db = wx.cloud.database();
 		db.collection("cities").get({
@@ -15,12 +18,14 @@ Page({
 			success:function(res){
 				that.setData({
 					cities:res.data
-				})
+				});
 			}
 		});
 		this.setData({
 			windowHeight:appInstance.windowHeight
-		})
+		});	
+
+		this.getSpotList(this.data.initCityName);
 	},
 
 	// 点击城市列表事件
@@ -34,5 +39,33 @@ Page({
 			  cities:this.data.cities,
 			  currentCityIndex:event.currentTarget.id
 		 });
+		 //获取查询城市名
+		 var targetCityName = this.data.cities[event.currentTarget.id].name;
+		 //console.log(targetCityName);
+		 this.getSpotList(targetCityName);
+
+	},
+	/**
+	 * 获取城市景点列表
+	 * @param {获取的目标景点的所在城市名称} cityName 
+	 */
+	getSpotList(cityName){
+		var that = this;
+		wx.cloud.callFunction({
+			name:"getSpotList",
+			data:{
+				"cityName":cityName
+			},
+			success:function(res){
+				//console.log(res.result.data);
+				that.setData({
+					spotList:res.result.data
+				});
+				//console.log(that.data.spotList);
+			},
+			fail:err =>{
+				console.log(err);
+			}
+		})
 	}
   });
