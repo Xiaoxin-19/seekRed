1a4de380a6001d4b3ba34338cc9519038b30fec4
何文鑫 <1641190800@qq.com>
2021-08-15 18:03:44 +0800

    1.我的足迹页面【mine】 定位测试代码移除 2.我的足迹页面【mine】 新增用户信息显示 头像，昵称，所在地 3.app.js 添加当前位置获取代码 4.文章详情页【article】 添加动态更新页面标题 5.首页【index】修改定位测试代码

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,11 @@
+var MapWX = require('./lib/qqmap-wx-jssdk1.2/qqmap-wx-jssdk.js');
+
+var mapsdk;
 App({
 
 	windowHeight : 100,
+	latitude:undefined,
+	longitude:undefined,
 /**
  * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
  */
@@ -13,33 +18,27 @@ App({
 		//获取用户可以屏幕高度
 		var that = this;
 		wx.getSystemInfo({
-			success: (result) => {
-				
+			success: (result) => {	
+				//console.log(this.windowHeight);	
 				this.windowHeight = result.windowHeight;
 				//console.log(result.windowHeight);
 			}
 		});
-	},
 
-/**
- * 当小程序启动，或从后台进入前台显示，会触发 onShow
- */
-	onShow: function (options) {
+		//实例化地图API和核心类
+		mapsdk = new MapWX({
+			key:'TH3BZ-M6JCV-3CAPI-URXZQ-D62YH-YYBKI'
+		})
 
-	},
-
-/**
- * 当小程序从前台进入后台，会触发 onHide
- */
-	onHide: function () {
-
-	},
-
-/**
- * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
- */
-	onError: function (msg) {
-
+		wx.getLocation({
+			type: 'gcj02',
+			isHighAccuracy:true,
+			success (res) {
+			 	//console.log(res.latitude);
+				that.latitude = res.latitude,
+				that.longitude  = res.longitude
+			}
+		});
 	}
 })
 
--- miniprogram/pages/article/article.js
+++ miniprogram/pages/article/article.js
@@ -13,7 +13,8 @@ Page({
 	 */
 	onLoad: function (options) {
 		var that = this;
-		console.log(options.id);
+		// console.log(options.id);
+		// 获取文章
 		wx.cloud.callFunction({
 			name:"getSpotArticle",
 			data:{
@@ -29,55 +30,13 @@ Page({
 				console.log(err);
 			}
 		})
-		
-	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
+		// 设置页面标题
+		//console.log(options.pageTitle);
+		wx.setNavigationBarTitle({
+			title: options.pageTitle,
+			fail: (err)=>{
+				console.log(err);
+			}
+		});
 	}
 })
